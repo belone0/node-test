f05878c91b062493337aad306117d440353c4141
beloner <joao.belone@edu.unifil.br>
2024-04-19 15:41:14 -0300

    host

--- server.js
+++ server.js
@@ -43,4 +43,7 @@ server.delete("/videos/:id", async (request, reply) => {
     return reply.status(204).send()
 })
 
-server.listen({ port: process.env.PORT ?? 3000})
+server.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ?? 3000
+    })
